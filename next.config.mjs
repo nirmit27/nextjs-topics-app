af76ea32066cf295be3d2603974185af35e1ca18
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-05-22 00:34:40 +0530

    fixing CORS issue (attempt 7)

--- next.config.mjs
+++ next.config.mjs
@@ -20,7 +20,7 @@ const nextConfig = {
         ],
       },
       {
-        source: "/api/addTopic/(.*)",
+        source: "api/topics/:id",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
