cb80a76aed7c0f31737366b3b0afa8e5a6fb0cb0
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-05-22 01:25:07 +0530

    ...

--- next.config.mjs
+++ next.config.mjs
@@ -20,7 +20,7 @@ const nextConfig = {
         ],
       },
       {
-        source: "/api/topics/",
+        source: "/api/topics",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
