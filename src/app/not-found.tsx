4b3d04915688798fafb419464006be21a3ad82c8
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-05-21 23:20:18 +0530

    not-found page setting

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,8 @@
-// Custom Not-Found page for the whole app
-
 export default function NotFound() {
   return (
     <div className="flex justify-center items-center flex-col mt-3 bg-white p-8 mx-auto max-w-3xl gap-2 rounded-md">
       <h1 className="text-red-500 font-semibold text-2xl">Error 404</h1>
-      <p>Couldn't find the requested resources.</p>
+      <p>Could not find the requested resources.</p>
     </div>
   );
 }
