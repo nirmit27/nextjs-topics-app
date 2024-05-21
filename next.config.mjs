ab0c0703afd35887481a05b8f96f9efe048c8c87
Nirmit Mishra <mishrayyhsnm2002@gmail.com>
2024-05-22 01:14:48 +0530

    recreating the issue

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/(.*)/(.*)",
+        source: "/",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
